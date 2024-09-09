93a1b18118f680ce06ca36dd9785b1b16c8b19bc
fetsorn <fetsorn@gmail.com>
2024-09-10 03:59:29 +0500

    export module

--- demo/README.md
+++
@@ -1,2 +0,0 @@
-The files in this directory are ad-hoc test and examples of how the code may be used.
-There is no expectation that they cover all use cases or test all code paths.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { GEDCStruct, g5ConfGEDC, g7ConfGEDC, GEDCToString } from "./gedcstruct.js";
+export { G7Date, G7DateValue, G7Enum, G7Datatype, checkDatatype } from "./g7datatypes.js"
+export { G7Structure, G7Dataset } from "./g7structure.js"
+export { G7Lookups } from "./g7lookups.js";
+export { g7validation } from "./g7validation.js";
